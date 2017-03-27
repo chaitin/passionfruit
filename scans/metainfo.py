9c16a37033b95cb8da6a62e75a206bb3bd26a587
codecolorist <codecolorist@gmail.com>
2017-03-27 22:07:25 +0800

    dump plist as xml

--- scans/metainfo.py
+++ scans/metainfo.py
@@ -5,10 +5,17 @@ from core.manifest import load as load_meta
 
 # todo: cache meta somewhere
 def scan(directory):
-  urls = load_meta(directory).urls
+  manifest = load_meta(directory)
+  urls = manifest.urls
   yield {
     'urls': urls,
     'msg': 'found following urls: \n%s' % '\n'.join(urls)
+  }
+
+  yield {
+    'raw': plistlib.dumps(manifest),
+    'level': 'message',
+    'msg': 'content of Info.plist',
   }
 
   # todo: url fuzzer!
