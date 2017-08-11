340a95b44bf1b6d4a9aa3dc8873bd05464931c7d
codecolorist <codecolorist@gmail.com>
2017-08-11 14:27:48 +0800

    indent

--- syringe/agent.test.js
+++ syringe/agent.test.js
@@ -9,57 +9,56 @@ async function sleep(ms) {
 }
 
 async function main(filename) {
-    let dev = await frida.getUsbDevice()
-    let apps = await dev.enumerateApplications()
+  let dev = await frida.getUsbDevice()
+  let apps = await dev.enumerateApplications()
 
-    let targetApp = apps.find(app => app.pid == 0)
-    if (!targetApp) {
-      throw Error('failed to find a app to launch')
-    }
+  let targetApp = apps.find(app => app.pid == 0)
+  if (!targetApp) {
+    throw Error('failed to find a app to launch')
+  }
 
-    console.info(`spawn ${targetApp.identifier}`)
+  console.info(`spawn ${targetApp.identifier}`)
 
-    let pid = await dev.spawn([targetApp.identifier])
-    let session = await dev.attach(pid)
-    console.info('app launching...')
-    await dev.resume(pid)
+  let pid = await dev.spawn([targetApp.identifier])
+  let session = await dev.attach(pid)
+  console.info('app launching...')
+  await dev.resume(pid)
 
-    let probeScript = await session.createScript('rpc.exports.ok = function() { return true }')
-    await probeScript.load()
-    let probe = await probeScript.getExports()
-    let retry = 10
-    while(--retry > 0) {
-      sleep(200)
-      console.debug('retry:', retry)
-      try {
-        if (await probe.ok()) {
-          console.debug('ok')
-          break
-        }
-      } catch(ignored) {}
-    }
-
-    if (retry == 0) {
-      console.error(`failed to spawn or inject into ${targetApp.identifier}`)
-      return await session.detach()
-    }
-
-    let source = await fridaLoad(require.resolve('./agent/' + filename))
-    let script = await session.createScript(source)
-
-    await script.load()
-    let api = await script.getExports()
+  let probeScript = await session.createScript('rpc.exports.ok = function() { return true }')
+  await probeScript.load()
+  let probe = await probeScript.getExports()
+  let retry = 10
+  while(--retry > 0) {
+    sleep(200)
+    console.debug('retry:', retry)
     try {
-      console.log(await api.main())
-    } catch(e) {
-      console.error(`unable execute script`)
-      console.error(e)
-    } finally {
-      console.log('clean up')
-      await dev.kill(pid)
-      await session.detach()
-    }
+      if (await probe.ok()) {
+        console.debug('ok')
+        break
+      }
+    } catch(ignored) {}
+  }
 
+  if (retry == 0) {
+    console.error(`failed to spawn or inject into ${targetApp.identifier}`)
+    return await session.detach()
+  }
+
+  let source = await fridaLoad(require.resolve('./agent/' + filename))
+  let script = await session.createScript(source)
+
+  await script.load()
+  let api = await script.getExports()
+  try {
+    console.log(await api.main())
+  } catch(e) {
+    console.error(`unable execute script`)
+    console.error(e)
+  } finally {
+    console.log('clean up')
+    await dev.kill(pid)
+    await session.detach()
+  }
 }
 
 process.on('unhandledRejection', error => {
