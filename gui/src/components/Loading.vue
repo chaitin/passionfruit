13201b00c98acb973ee7775d812b1c6da9fc5f12
codecolorist <codecolorist@gmail.com>
2017-09-06 01:19:45 +0800

    add an inline-block loading cmponent

---
+++ gui/src/components/Loading.vue
@@ -0,0 +1,45 @@
+<template>
+  <figure class="button is-loading is-primary is-outlined no-border" :style="css"></figure>
+</template>
+
+<script>
+export default {
+  props: ['height', 'width', 'radius'],
+  computed: {
+    fontSize() {
+      let { radius } = this
+      if (!radius)
+        return '1em'
+
+      const matched = /^(\d+(?:\.\d+)?)(%|px|em|rem|vh|wh)$/.exec(radius)
+      if (!matched)
+        throw new Error(`invalid radius expression: ${radius}`)
+
+      let [_, number, unit] = matched
+      let val = +number
+      if (isNaN(val))
+        throw new Error(`invalid radius size: ${number}`)
+
+      if (unit == 'px')
+        return val / 16 + 'em'
+      else if (unit == '%')
+        return val / 100 + 'em'
+      else
+        return radius
+    },
+    css() {
+      return {
+        width: this.width,
+        height: this.height,
+        fontSize: this.fontSize,
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.no-border {
+  border: none;
+}
+</style>
