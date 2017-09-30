b8097352c99fad130774909099ae436650c35068
codecolorist <codecolorist@gmail.com>
2017-09-30 15:07:04 +0800

    dump class prototype chain

--- frida/classdump.js
+++ frida/classdump.js
@@ -2,7 +2,24 @@ exports.classes = function() {
   return Object.keys(ObjC.classes)
 }
 
-exports.methods = function(clazz) {
+exports.methods = function methods(clazz) {
   return ObjC.classes[clazz].$ownMethods
 }
 
+exports.proto = function proto(clazz) {
+  let chain = []
+  let clz = ObjC.classes[clazz]
+  if (!clz)
+    throw new Error(`class ${clazz} not found`)
+
+  while (clz = clz.$superClass)
+    chain.push(clz.$className)
+  return chain
+}
+
+exports.inspect = function(clazz) {
+  return {
+    methods: methods(clazz),
+    proto: proto(clazz),
+  }
+}
--- frida/index.js
+++ frida/index.js
@@ -6,7 +6,7 @@ import urlOpen from './ipc'
 import keychain from './keychain'
 
 import { info } from './info'
-import { classes, methods } from './classdump'
+import { classes, methods, inspect, proto } from './classdump'
 import { tables, data, query } from './sqlite'
 import { ls, home, plist, text, download } from './finder'
 import { dumpWindow, toggleTouchID } from './ui'
@@ -16,6 +16,7 @@ import { hook, unhook, swizzle, unswizzle } from './hook'
 toggleTouchID(false)
 // hook('/usr/lib/libSystem.B.dylib', 'open', { args: ['char *', 'int'], ret: 'int'})
 swizzle('NSURL', 'URLWithString_')
+swizzle('NSString', 'stringWithContentsOfFile_usedEncoding_error_')
 
 
 rpc.exports = {
@@ -25,6 +26,8 @@ rpc.exports = {
   lsof,
   classes,
   methods,
+  inspect,
+  proto,
   imports,
 
   ls,
