abe3dd7e1f68f9e59656155f87bf78c0a4e0571f
codecolorist <codecolorist@gmail.com>
2017-10-01 00:08:02 +0800

    fix binarycookie

--- frida/binarycookie.js
+++ frida/binarycookie.js
@@ -1,27 +1,22 @@
-const NSHTTPCookieStorage = ObjC.classes.NSHTTPCookieStorage;
+const { NSHTTPCookieStorage } = ObjC.classes
 
-const store = NSHTTPCookieStorage.sharedHTTPCookieStorage();
-const jar = store.cookies();
-
+const store = NSHTTPCookieStorage.sharedHTTPCookieStorage()
+const jar = store.cookies()
 
 function str(obj, def) {
-  return obj ? obj.toString() : (def || 'N/A');
+  return obj ? obj.toString() : (def || 'N/A')
 }
 
 module.exports = function() {
   let cookies = []
 
   for (let i = 0; i < jar.count(); i++) {
-    let cookie = jar.objectAtIndex_(i);
+    let cookie = jar.objectAtIndex_(i)
     let item = {
       version: cookie.version().toString(),
       name: cookie.name().toString(),
       value: cookie.value().toString(),
-      expiresDate: str(cookie.expiresDate()),
-      created: cookie.created().toString(),
-      sessionOnly: str(cookie.sessionOnly(), false),
       domain: cookie.domain().toString(),
-      partition: str(cookie.partition()),
       path: cookie.path().toString(),
       isSecure: str(cookie.isSecure(), 'false')
     }
