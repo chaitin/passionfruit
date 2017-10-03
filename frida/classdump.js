c385fb3b143384331cb384869cf309377e7eb769
codecolorist <codecolorist@gmail.com>
2017-10-03 12:54:04 +0800

    display only classes of its own

--- frida/classdump.js
+++ frida/classdump.js
@@ -1,5 +1,41 @@
+function getOwnClasses(sort) {
+  const free = new NativeFunction(Module.findExportByName(null, 'free'), 'void', ['pointer'])
+  const objc_copyClassNamesForImage = new NativeFunction(Module.findExportByName(
+    null, 'objc_copyClassNamesForImage'), 'pointer', ['pointer', 'pointer'])
+  const classes = new Array(count)
+  const p = Memory.alloc(Process.pointerSize)
+  Memory.writeUInt(p, 0)
+  const path = ObjC.classes.NSBundle.mainBundle().executablePath().UTF8String()
+  const pPath = Memory.allocUtf8String(path)
+  const pClasses = objc_copyClassNamesForImage(pPath, p)
+  const count = Memory.readUInt(p)
+  for (let i = 0; i < count; i++) {
+    let pClassName = Memory.readPointer(pClasses.add(i * Process.pointerSize))
+    classes[i] = Memory.readUtf8String(pClassName)
+  }
+  free(pClasses)
+  return sort ? classes.sort() : classes
+}
+
+function getGlobalClasses(sort) {
+  const classes = Object.keys(ObjC.classes)
+  return sort ? classes.sort() : classes
+}
+
+const ownClasses = null
+const globalClasses = null
+
+exports.ownClasses = function() {
+  if (!ownClasses)
+    ownClasses = getOwnClasses(true)
+  return ownClasses
+}
+
 exports.classes = function() {
-  return Object.keys(ObjC.classes)
+  if (!globalClasses)
+    globalClasses = getGlobalClasses(true)
+  
+  return globalClasses
 }
 
 exports.methods = function methods(clazz) {
