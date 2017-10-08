63a829b34385801f546594d4772133f0b060fa71
codecolorist <codecolorist@gmail.com>
2017-10-08 23:58:30 +0800

    native function hook ui
    set arguments type

--- gui/src/components/Functions.vue
+++ gui/src/components/Functions.vue
@@ -14,28 +14,48 @@
     <b-pagination :total="filtered.length" :current.sync="page" order="is-centered" :per-page="paginator"></b-pagination>
 
     <b-modal :active.sync="symbolDialogActive" :width="640">
-      <div class="card" v-if="symbol">
-        <div class="card-content">
-          <div class="media">
-            <div class="media-content">
-              <p class="title is-4">{{ module }}!{{ symbol.name }}</p>
-              <p class="subtitle is-6">
-                <span v-if="symbol.address.value">
-                  0x{{ symbol.address.value.toString(16) }}</span>
-                <span v-else>{{ symbol.address }}</span>
-              </p>
+      <div class="modal-card" v-if="symbol">
+        <header class="modal-card-head">
+          <p class="modal-card-title">{{ module }}!{{ symbol.name }}</p>
+        </header>
 
-              <p>Todo: Set argument types</p>
-              <p>
-                <a class="button">
-                  <b-icon icon="add"></b-icon>
-                  <span>Add to Interceptor</span>
-                </a>
-              </p>
-            </div>
-          </div>
+        <section class="modal-card-body">
+          <!-- <p class="subtitle is-6">
+              <span v-if="symbol.address.value">
+                0x{{ symbol.address.value.toString(16) }}</span>
+              <span v-else>{{ symbol.address }}</span>
+            </p> -->
 
-        </div>
+          <p>Arguments</p>
+
+          <b-field v-for="(arg, index) in args" :key="index">
+            <b-select placeholder="Argument type" v-model="arg.type" expanded>
+              <option v-for="(t, j) in types" :key="j" :value="t">{{ t }}</option>
+            </b-select>
+            <p class="control">
+              <button class="button is-danger" @click="removeArg(index)">
+                <b-icon icon="remove_circle_outline"></b-icon>
+              </button>
+            </p>
+          </b-field>
+
+          <b-field>
+            <button @click="addArg" class="button">
+              <b-icon icon="add_circle_outline"></b-icon>
+              <span>Add</span>
+            </button>
+          </b-field>
+
+          <b-field>
+            <code>{{ expr }}</code>
+          </b-field>
+
+        </section>
+
+        <footer class="modal-card-foot confirm-footer">
+          <button class="button" type="button" @click="symbolDialogActive = false">Dismiss</button>
+          <button class="button is-primary" @click="hook">Send to Interceptor</button>
+        </footer>
       </div>
     </b-modal>
 
@@ -88,16 +108,36 @@ export default {
       // selected symbol
       symbolDialogActive: false,
       symbol: null,
+      args: [],
+      types: ['int', 'uint', 'long', 'ulong', 'char', 'uchar', 'char *',
+        'int8', 'uint8', 'int16', 'uint16', 'int32', 'uint32', 'int64', 'uint64'],
+      ret: 'void',
     }
   },
   methods: {
     openSymbolDetail(symbol) {
       this.symbolDialogActive = true
+      this.args = []
+      this.ret = 'void'
       this.symbol = symbol
+    },
+    addArg() {
+      this.args.push({ type: 'char *' })
+    },
+    removeArg(index) {
+      this.args.splice(index, 1)
     },
     paginate(page, filtered, paginator) {
       this.slice = filtered.slice((page - 1) * paginator, page * paginator)
     },
+    hook() {
+      // todo
+    },
+  },
+  computed: {
+    expr() {
+      return this.symbol.name + '(' + this.args.map(t => t.type).join(', ') + ')'
+    }
   },
   mounted() {
     this.matcher = new AsyncSearch(this.list, 'name').onMatch(result => {
@@ -139,5 +179,9 @@ ul.functions {
       cursor: pointer;
     }
   }
+}
+
+.confirm-footer {
+  justify-content: flex-end;
 }
 </style>
