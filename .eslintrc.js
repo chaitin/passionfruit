0fbd03ee59e9acb5d5493cc437a6a1db222cd74b
codecolorist <codecolorist@gmail.com>
2017-10-11 17:19:04 +0800

    add eslint (more source file to be covered)

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: 'airbnb-base',
+  rules: {
+    semi: 0,
+    'no-console': 0,
+    'class-methods-use-this': 0,
+    'space-before-function-paren': 0,
+    'object-curly-newline': 0,
+    'one-var': 0,
+    'one-var-declaration-per-line': 0,
+    'no-restricted-syntax': 0,
+    'no-await-in-loop': 0,
+    'no-plusplus': 0,
+    'import/no-unresolved': 0,
+    curly: ['error', 'multi', 'consistent'],
+  }
+};
--- gui/src/main.js
+++ gui/src/main.js
@@ -1,24 +1,23 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
 import io from 'socket.io-client'
 import Buefy from 'buefy'
 import 'buefy/lib/buefy.css'
 import axios from 'axios'
 
+import App from '~/App.vue'
+import router from '~/router'
+import store from '~/vuex'
+import { humanFileSize } from '~/lib/utils'
+
+
+import 'material-design-icons/iconfont/material-icons.css'
+
+import { ADD_DEVICE, REMOVE_DEVICE } from '~/vuex/types'
+
 //
 require('promise.prototype.finally').shim()
 axios.defaults.baseURL = '/api'
 
-
-import App from '~/App.vue'
-import router from '~/router'
-import store from '~/vuex'
-import { humanFileSize } from '~/lib/utils.js'
-
-
-import "material-design-icons/iconfont/material-icons.css"
-
-import { ADD_DEVICE, REMOVE_DEVICE } from '~/vuex/types'
 
 Vue.use(Buefy)
 Vue.filter('filesize', humanFileSize)
@@ -28,16 +27,16 @@ const v = new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
 })
 
 const socket = io('/devices', { path: '/msg' })
 socket
-  .on('deviceRemove', dev => {
+  .on('deviceRemove', (dev) => {
     store.commit(REMOVE_DEVICE, dev)
     v.$toast.open(`${dev.name} has been removed`)
   })
-  .on('deviceAdd', dev => {
+  .on('deviceAdd', (dev) => {
     store.commit(ADD_DEVICE, dev)
     v.$toast.open(`New device ${dev.name} has been connected`)
   })
