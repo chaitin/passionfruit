9ff0efa35e4fdcf22b72ab0944a31e0df22f9295
codecolorist <codecolorist@gmail.com>
2017-10-13 11:51:53 +0800

    add springboard agent

---
+++ frida/springboard.js
@@ -0,0 +1,35 @@
+const {
+  SBBacklightController,
+  SBLockScreenManager,
+  SBApplicationController,
+  SBUIController
+} = ObjC.classes
+
+
+const perform = f => ObjC.schedule(ObjC.mainQueue, f)
+
+rpc.exports = {
+  unlock() {
+    perform(() => {
+      SBBacklightController.sharedInstance().turnOnScreenFullyWithBacklightSource_(0)
+      SBLockScreenManager.sharedInstance().unlockUIFromSource_withOptions_(0, null)
+    })
+  },
+
+  uiopen(url) {
+    const { LSApplicationWorkspace, NSURL } = ObjC.classes
+    const workspace = LSApplicationWorkspace.defaultWorkspace()
+    const link = NSURL.URLWithString_(url)
+    return workspace.openSensitiveURL_withOptions_(link, NULL)
+  },
+
+  activate(bundle) {
+    perform(() => {
+      const controller = SBApplicationController.sharedInstance()
+      const app = typeof controller.applicationWithBundleIdentifier_ === 'function' ?
+        controller.applicationWithBundleIdentifier_(bundle) : // iOS 8+
+        controller.applicationWithDisplayIdentifier_(bundle) // iOS 7-
+      SBUIController.sharedInstanceIfExists().activateApplication_(app)
+    })
+  }
+}
