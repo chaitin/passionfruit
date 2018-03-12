8b648ceeb7ceb6cccfcf252914f1be89a2f731a8
codecolorist <codecolorist@gmail.com>
2018-03-12 14:24:09 +0800

    fix #21 speed up app launch by sending heavy hooks to async queue

--- agent/app/index.js
+++ agent/app/index.js
@@ -4,6 +4,7 @@ import cookies from './binarycookie'
 import keychain from './keychain'
 import dumpdecrypted from './dumpdecrypted'
 import screenshot from './screenshot'
+import bypassJailbreak from './jailbreak'
 
 
 import { info, userDefaults } from './info'
@@ -16,26 +17,25 @@ import { hook, unhook, swizzle, unswizzle } from './hook'
 
 // todo: add options
 
-require('./jailbreak') // try bypass jailbreak
 require('./pasteboard') // monitor pasteboard
 
-toggleTouchID(false)
+setTimeout(() => {
+  Module.ensureInitialized('Foundation')
 
-/*
+  toggleTouchID(false) // try bypass jailbreak
+  bypassJailbreak(true)
 
-disable to default hooks speed up
+  // todo: common function template
+  hook('libSystem.B.dylib', 'open', { args: ['char *', 'int'] })
+  hook('libsqlite3.dylib', 'sqlite3_open', { args: ['char *', 'int'], ret: 'int' })
+  hook('libsqlite3.dylib', 'sqlite3_prepare_v2', { args: ['pointer', 'char *', 'int', 'pointer', 'pointer'] })
+  hook('libsqlite3.dylib', 'sqlite3_bind_int', { args: ['pointer', 'int', 'int'] })
+  hook('libsqlite3.dylib', 'sqlite3_bind_null', { args: ['pointer', 'int'] })
+  hook('libsqlite3.dylib', 'sqlite3_bind_text', { args: ['pointer', 'int', 'char *', 'int', 'pointer'] })
 
-// hook('libSystem.B.dylib', 'open', { args: ['char *', 'int']})
-
-hook('libsqlite3.dylib', 'sqlite3_open', { args: ['char *', 'int'], ret: 'int' })
-hook('libsqlite3.dylib', 'sqlite3_prepare_v2', { args: ['pointer', 'char *', 'int', 'pointer', 'pointer'] })
-hook('libsqlite3.dylib', 'sqlite3_bind_int', { args: ['pointer', 'int', 'int'] })
-hook('libsqlite3.dylib', 'sqlite3_bind_null', { args: ['pointer', 'int'] })
-hook('libsqlite3.dylib', 'sqlite3_bind_text', { args: ['pointer', 'int', 'char *', 'int', 'pointer'] })
-
-// swizzle('NSURL', 'URLWithString_', false)
-swizzle('NSString', 'stringWithContentsOfFile_usedEncoding_error_')
-*/
+  swizzle('NSURL', 'URLWithString_', false)
+  swizzle('NSString', 'stringWithContentsOfFile_usedEncoding_error_')
+}, 1000)
 
 rpc.exports = {
   checksec,
--- agent/app/jailbreak.js
+++ agent/app/jailbreak.js
@@ -32,171 +32,172 @@ const paths = `/Applications/Cydia.app
 
 const subject = 'jailbreak'
 
-/* eslint no-param-reassign: 0, camelcase: 0, eqeqeq: 0, prefer-destructuring: 0 */
-Interceptor.attach(Module.findExportByName(null, 'open'), {
-  onEnter(args) {
-    if (!args[0])
-      return
+export default function bypassJailbreak() {
+  /* eslint no-param-reassign: 0, camelcase: 0, eqeqeq: 0, prefer-destructuring: 0 */
+  Interceptor.attach(Module.findExportByName(null, 'open'), {
+    onEnter(args) {
+      if (!args[0])
+        return
 
-    const backtrace = Thread.backtrace(this.context, Backtracer.ACCURATE).map(
-      DebugSymbol.fromAddress).filter(e => e.name)
-    const path = Memory.readUtf8String(args[0])
-    if (paths.indexOf(path) > -1) {
-      send({
-        subject,
-        time: new Date().getTime(),
-        event: 'detect',
-        backtrace,
-        arguments: {
-          path,
-          method: 'open',
-        },
-        backtrace,
-      })
-      args[0] = NULL
-    }
-  },
-})
+      const backtrace = Thread.backtrace(this.context, Backtracer.ACCURATE).map(
+        DebugSymbol.fromAddress).filter(e => e.name)
+      const path = Memory.readUtf8String(args[0])
+      if (paths.indexOf(path) > -1) {
+        send({
+          subject,
+          time: new Date().getTime(),
+          event: 'detect',
+          backtrace,
+          arguments: {
+            path,
+            method: 'open',
+          },
+        })
+        args[0] = NULL
+      }
+    },
+  })
 
-Interceptor.attach(Module.findExportByName(null, 'stat'), {
-  onEnter(args) {
-    if (!args[0])
-      return
+  Interceptor.attach(Module.findExportByName(null, 'stat'), {
+    onEnter(args) {
+      if (!args[0])
+        return
 
-    const path = Memory.readUtf8String(args[0])
-    const backtrace = Thread.backtrace(this.context, Backtracer.ACCURATE).map(
-      DebugSymbol.fromAddress).filter(e => e.name)
-    if (paths.indexOf(path) > -1) {
-      send({
-        subject,
-        time: new Date().getTime(),
-        event: 'detect',
-        backtrace,
-        arguments: {
-          path,
-          method: 'stat',
-        },
-      })
-      args[0] = NULL
-    }
-  },
-})
+      const path = Memory.readUtf8String(args[0])
+      const backtrace = Thread.backtrace(this.context, Backtracer.ACCURATE).map(
+        DebugSymbol.fromAddress).filter(e => e.name)
+      if (paths.indexOf(path) > -1) {
+        send({
+          subject,
+          time: new Date().getTime(),
+          event: 'detect',
+          backtrace,
+          arguments: {
+            path,
+            method: 'stat',
+          },
+        })
+        args[0] = NULL
+      }
+    },
+  })
 
-Interceptor.attach(Module.findExportByName(null, 'getenv'), {
-  onEnter(args) {
-    const key = Memory.readUtf8String(args[0])
-    const backtrace = Thread.backtrace(this.context, Backtracer.ACCURATE).map(
-      DebugSymbol.fromAddress).filter(e => e.name)
-    if (key === 'DYLD_INSERT_LIBRARIES') {
-      send({
-        subject,
-        time: new Date().getTime(),
-        event: 'detect',
-        backtrace,
-        arguments: {
-          env: 'DYLD_INSERT_LIBRARIES',
-        },
-      })
-      args[0] = NULL
-    }
-  },
-})
+  Interceptor.attach(Module.findExportByName(null, 'getenv'), {
+    onEnter(args) {
+      const key = Memory.readUtf8String(args[0])
+      const backtrace = Thread.backtrace(this.context, Backtracer.ACCURATE).map(
+        DebugSymbol.fromAddress).filter(e => e.name)
+      if (key === 'DYLD_INSERT_LIBRARIES') {
+        send({
+          subject,
+          time: new Date().getTime(),
+          event: 'detect',
+          backtrace,
+          arguments: {
+            env: 'DYLD_INSERT_LIBRARIES',
+          },
+        })
+        args[0] = NULL
+      }
+    },
+  })
 
-Interceptor.attach(Module.findExportByName(null, '_dyld_get_image_name'), {
-  onLeave(retVal) {
-    if (Memory.readUtf8String(retVal).indexOf('MobileSubstrate') > -1)
-      retVal.replace(ptr(0x00))
-  },
-})
+  Interceptor.attach(Module.findExportByName(null, '_dyld_get_image_name'), {
+    onLeave(retVal) {
+      if (Memory.readUtf8String(retVal).indexOf('MobileSubstrate') > -1)
+        retVal.replace(ptr(0x00))
+    },
+  })
 
-Interceptor.attach(Module.findExportByName(null, 'fork'), {
-  onLeave(retVal) {
-    retVal.replace(ptr(-1))
-    // todo: send
-  },
-})
+  Interceptor.attach(Module.findExportByName(null, 'fork'), {
+    onLeave(retVal) {
+      retVal.replace(ptr(-1))
+      // todo: send
+    },
+  })
 
-const { UIApplication, NSURL, NSString, NSError, NSFileManager } = ObjC.classes
-const canOpenURL_publicURLsOnly_ = UIApplication['- _canOpenURL:publicURLsOnly:']
-Interceptor.attach(canOpenURL_publicURLsOnly_.implementation, {
-  onEnter(args) {
-    if (args[2] == '0x0')
-      return
+  const { UIApplication, NSURL, NSString, NSError, NSFileManager } = ObjC.classes
+  const canOpenURL_publicURLsOnly_ = UIApplication['- _canOpenURL:publicURLsOnly:']
+  Interceptor.attach(canOpenURL_publicURLsOnly_.implementation, {
+    onEnter(args) {
+      if (args[2] == '0x0')
+        return
 
-    const url = ObjC.Object(args[2]).toString()
-    const backtrace = Thread.backtrace(this.context, Backtracer.ACCURATE).map(
-      DebugSymbol.fromAddress).filter(e => e.name)
-    if (/^cydia:\/\//i.exec(url)) {
-      args[2] = NSURL.URLWithString_('invalid://')
-      this.shouldOverride = true
-      send({
-        subject,
-        time: new Date().getTime(),
-        event: 'detect',
-        backtrace,
-        arguments: {
-          url,
-        },
-      })
-    }
-  },
-  onLeave(retVal) {
-    if (this.shouldOverride)
-      retVal.replace(ptr('0x00'))
-  },
-})
+      const url = ObjC.Object(args[2]).toString()
+      const backtrace = Thread.backtrace(this.context, Backtracer.ACCURATE).map(
+        DebugSymbol.fromAddress).filter(e => e.name)
+      if (/^cydia:\/\//i.exec(url)) {
+        args[2] = NSURL.URLWithString_('invalid://')
+        this.shouldOverride = true
+        send({
+          subject,
+          time: new Date().getTime(),
+          event: 'detect',
+          backtrace,
+          arguments: {
+            url,
+          },
+        })
+      }
+    },
+    onLeave(retVal) {
+      if (this.shouldOverride)
+        retVal.replace(ptr('0x00'))
+    },
+  })
 
-Interceptor.attach(NSFileManager['- fileExistsAtPath:'].implementation, {
-  onEnter(args) {
-    if (args[2] == '0x0')
-      return
+  Interceptor.attach(NSFileManager['- fileExistsAtPath:'].implementation, {
+    onEnter(args) {
+      if (args[2] == '0x0')
+        return
 
-    const path = new ObjC.Object(args[2]).toString()
-    const backtrace = Thread.backtrace(this.context, Backtracer.ACCURATE).map(
-      DebugSymbol.fromAddress).filter(e => e.name)
-    if (paths.indexOf(path) > -1) {
-      send({
-        subject,
-        time: new Date().getTime(),
-        event: 'detect',
-        backtrace,
-        arguments: {
-          path,
-        },
-      })
-      this.shouldOverride = true
-    }
-  },
-  onLeave(retVal) {
-    if (this.shouldOverride)
-      retVal.replace(ptr('0x00'))
-  },
-})
+      const path = new ObjC.Object(args[2]).toString()
+      const backtrace = Thread.backtrace(this.context, Backtracer.ACCURATE).map(
+        DebugSymbol.fromAddress).filter(e => e.name)
+      if (paths.indexOf(path) > -1) {
+        send({
+          subject,
+          time: new Date().getTime(),
+          event: 'detect',
+          backtrace,
+          arguments: {
+            path,
+          },
+        })
+        this.shouldOverride = true
+      }
+    },
+    onLeave(retVal) {
+      if (this.shouldOverride)
+        retVal.replace(ptr('0x00'))
+    },
+  })
 
-Interceptor.attach(NSString['- writeToFile:atomically:encoding:error:'].implementation, {
-  onEnter(args) {
-    if (args[2] == '0x0')
-      return
+  Interceptor.attach(NSString['- writeToFile:atomically:encoding:error:'].implementation, {
+    onEnter(args) {
+      if (args[2] == '0x0')
+        return
 
-    const path = ObjC.Object(args[2]).toString()
-    const backtrace = Thread.backtrace(this.context, Backtracer.ACCURATE).map(
-      DebugSymbol.fromAddress).filter(e => e.name)
-    if (path.match(/^\/private/)) {
-      send({
-        subject,
-        time: new Date().getTime(),
-        event: 'detect',
-        backtrace,
-        arguments: {
-          path,
-        },
-      })
-      this.shouldOverride = true
-      this.error = args[5]
-    }
-  },
-  onLeave() {
-    if (this.shouldOverride)
-      Memory.writePointer(this.error, NSError.alloc())
-  },
-})
+      const path = ObjC.Object(args[2]).toString()
+      const backtrace = Thread.backtrace(this.context, Backtracer.ACCURATE).map(
+        DebugSymbol.fromAddress).filter(e => e.name)
+      if (path.match(/^\/private/)) {
+        send({
+          subject,
+          time: new Date().getTime(),
+          event: 'detect',
+          backtrace,
+          arguments: {
+            path,
+          },
+        })
+        this.shouldOverride = true
+        this.error = args[5]
+      }
+    },
+    onLeave() {
+      if (this.shouldOverride)
+        Memory.writePointer(this.error, NSError.alloc())
+    },
+  })
+}
