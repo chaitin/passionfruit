4fa8f21f0ad8a54a711f34c6a77112e041d151a7
codecolorist <codecolorist@gmail.com>
2018-03-29 18:26:01 +0800

    hopefully fix #13

--- agent/app/ui.js
+++ agent/app/ui.js
@@ -9,46 +9,43 @@ let originalImplementation = null
 
 function toggleTouchID(enable) {
   const subject = 'touchid'
-  if (!LAContext) {
-    return {
-      status: 'error',
-      reason: 'Touch ID may not be supported by this device',
-    }
-  }
+  if (!LAContext)
+    throw new Error('Touch ID may not be supported by this device')
 
   const method = LAContext['- evaluatePolicy:localizedReason:reply:']
-  if (originalImplementation && !enable) {
+  if (originalImplementation && enable) {
     method.implementation = originalImplementation
     originalImplementation = null
-    return {
-      status: 'ok',
-      reason: 'Successfully re-enabled touch id',
-    }
-  } else if (!originalImplementation && enable) {
+
+    send({
+      subject,
+      event: 'on',
+      reason: 're-eanbled touch id',
+      date: new Date(),
+    })
+  } else if (!originalImplementation && !enable) {
     originalImplementation = method.implementation
     method.implementation = ObjC.implement(method, (self, sel, policy, reason, reply) => {
-      const backtrace = Thread.backtrace(this.context, Backtracer.ACCURATE)
-        .map(DebugSymbol.fromAddress).filter(e => e.name)
-
       send({
         subject,
         event: 'request',
         reason,
-        backtrace,
+        date: new Date(),
       })
 
       // dismiss the dialog
       const callback = new ObjC.Block(ptr(reply))
       callback.implementation(1, null)
     })
-    return {
-      status: 'ok',
-      reason: 'Successfully disabled touch id',
-    }
-  }
-  return {
-    status: 'error',
-    reason: 'invalid on/off argument',
+
+    send({
+      subject,
+      event: 'off',
+      reason: 'successfully disabled touch id',
+      date: new Date(),
+    })
+  } else {
+    throw new Error('invalid on/off argument')
   }
 }
 
