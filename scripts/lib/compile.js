71836c0698317fd782f1f8c7f443b9533b5e68bc
CodeColorist <codecolorist@gmail.com>
2019-07-04 00:14:52 +0800

    fix #51

--- scripts/lib/compile.js
+++ scripts/lib/compile.js
@@ -3,13 +3,13 @@ const compiler = require('frida-compile')
 const TAG_FRIDA = '[Frida]'.yellow
 const tasks = [{
   src: 'agent/app/index.js',
-  dest: 'agent/app.bundle',
+  dest: 'agent/app.bundle.js',
 }]
 
 
 exports.run = function(watch) {
   const opt = {
-    bytecode: !watch,
+    bytecode: false,
     compress: false,
     babelify: true,
     sourcemap: watch,
@@ -18,7 +18,7 @@ exports.run = function(watch) {
   }
 
   if (watch) {
-    tasks.forEach(task => compiler.watch(task.src, `${task.dest}.js`, opt)
+    tasks.forEach(task => compiler.watch(task.src, task.dest, opt)
       .on('compile', (details) => {
         const count = details.files.length
         const { duration } = details
@@ -26,7 +26,7 @@ exports.run = function(watch) {
       }))
   } else {
     Promise
-      .all(tasks.map(task => compiler.build(task.src, `${task.dest}.bin`, opt)))
+      .all(tasks.map(task => compiler.build(task.src, task.dest, opt)))
       .catch(err => console.error(err))
   }
 }
