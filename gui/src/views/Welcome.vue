60c6f73cc284353bb7bd8bf8a1bdeb5da6f12e8e
CodeColorist <codecolorist@gmail.com>
2020-03-21 00:10:17 +0800

    add device UI

--- gui/src/views/Welcome.vue
+++ gui/src/views/Welcome.vue
@@ -2,29 +2,40 @@
   <div class="container is-fluid">
     <div class="columns section">
       <div class="column is-one-quarter">
-        <h1 class="title has-text-grey-darker"><img class="logo" src="../assets/logo.svg" alt="Passionfruit"></h1>
+        <h1 class="title has-text-grey-darker">
+          <img class="logo" src="../assets/logo.svg" alt="Passionfruit" />
+        </h1>
         <aside class="menu">
+          <p class="menu-label">Frida version: {{ version }}</p>
           <p class="menu-label">
-            Frida version: {{ version }} 
-          </p>
-          <p class="menu-label">
-            Devices <loading v-if="loadingDevices" class="is-pulled-right"></loading>
+            Devices
+            <loading v-if="loadingDevices" class="is-pulled-right"></loading>
           </p>
           <ul class="menu-list">
             <li v-for="dev in devices" :key="dev.id">
               <router-link :to="{ name: 'apps', params: { device: dev.id } }">
-                <icon :icon="dev.icon" :width="24" :height="24"></icon> {{ dev.name }}
+                <icon :icon="dev.icon" :width="24" :height="24"></icon>
+                {{ dev.name }}
               </router-link>
             </li>
-            <li v-if="!devices.length"><b-icon icon="error"></b-icon> No device found</li>
+            <li v-if="!devices.length">
+              <b-icon icon="error"></b-icon>No device found
+            </li>
+            <li>
+              <b-button class="add" @click="connect" type="is-light">
+                <b-icon icon="add_box"></b-icon>Connect Remote
+              </b-button>
+            </li>
           </ul>
-          <p class="menu-label">
-            General
-          </p>
+          <p class="menu-label">General</p>
           <ul class="menu-list">
             <!-- <li><a><b-icon icon="settings"></b-icon> <span>Preference</span></a></li> -->
-            <li><a target="_blank" href="http://github.com/chaitin/passionfruit">
-              <b-icon icon="open_in_browser"></b-icon> <span>Github</span></a></li>
+            <li>
+              <a target="_blank" href="http://github.com/chaitin/passionfruit">
+                <b-icon icon="open_in_browser"></b-icon>
+                <span>Github</span>
+              </a>
+            </li>
           </ul>
         </aside>
       </div>
@@ -32,41 +43,63 @@
       <div class="column">
         <router-view></router-view>
       </div>
-
     </div>
   </div>
-
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
-import { GET_VERSION, GET_DEVICES, LOAD_DEVICES, DEVICES_LOADING } from '~/vuex/types'
-import Icon from '~/components/Icon.vue'
-import Loading from '~/components/Loading.vue'
+// TODO: refactor
+import axios from 'axios'
+
+import { mapGetters, mapActions } from "vuex";
+import {
+  GET_VERSION,
+  GET_DEVICES,
+  LOAD_DEVICES,
+  DEVICES_LOADING
+} from "~/vuex/types";
+import Icon from "~/components/Icon.vue";
+import Loading from "~/components/Loading.vue";
 
 export default {
   components: {
     Icon,
-    Loading,
+    Loading
   },
   computed: {
     ...mapGetters({
       version: GET_VERSION,
       devices: GET_DEVICES,
-      loadingDevices: DEVICES_LOADING,
+      loadingDevices: DEVICES_LOADING
     })
   },
   methods: {
+    connect() {
+      this.$dialog.prompt({
+        message: `IP address`,
+        inputAttrs: {
+          placeholder: "192.168.1.100",
+        },
+        trapFocus: true,
+        onConfirm(value) {
+          axios.put('/device/add', value)
+        }
+      });
+    },
     ...mapActions({
       refresh: LOAD_DEVICES
     })
   },
   mounted() {
-    this.refresh()
+    this.refresh();
   }
-}
+};
 </script>
 
-<style>
-
+<style scoped>
+.add {
+  padding: 0.5em 0.75em;
+  display: block;
+  cursor: pointer;
+}
 </style>
