a5617ae0d75092b55072c9b30911818f68c4d5ba
CodeColorist <codecolorist@gmail.com>
2020-03-21 00:50:16 +0800

    add UI for remote connection

--- gui/src/views/Welcome.vue
+++ gui/src/views/Welcome.vue
@@ -13,18 +13,21 @@
           </p>
           <ul class="menu-list">
             <li v-for="dev in devices" :key="dev.id">
-              <router-link :to="{ name: 'apps', params: { device: dev.id } }">
+              <router-link v-if="['local', 'tcp'].indexOf(dev.id) === -1" :to="{ name: 'apps', params: { device: dev.id } }">
                 <icon :icon="dev.icon" :width="24" :height="24"></icon>
                 {{ dev.name }}
+                <button v-if="dev.type === 'remote' " class="is-pulled-right remove button is-text"
+                  @click.stop.prevent="remove(dev.id)"><b-icon icon="remove_circle" type="is-danger"></b-icon></button>
               </router-link>
             </li>
             <li v-if="!devices.length">
-              <b-icon icon="error"></b-icon>No device found
+              <b-icon icon="button error"></b-icon>No device found
             </li>
             <li>
-              <b-button class="add" @click="connect" type="is-light">
-                <b-icon icon="add_box"></b-icon>Connect Remote
-              </b-button>
+              <a class="button add" @click="connect" type="is-light" icon-left="add_box">
+                <b-icon icon="add_box"></b-icon>
+                Connect Remote
+              </a>
             </li>
           </ul>
           <p class="menu-label">General</p>
@@ -76,13 +79,37 @@ export default {
   methods: {
     connect() {
       this.$dialog.prompt({
-        message: `IP address`,
+        message: `Connect Remote Device via TCP`,
         inputAttrs: {
           placeholder: "192.168.1.100",
         },
         trapFocus: true,
         onConfirm(value) {
           axios.put('/device/add', value)
+        }
+      });
+    },
+    remove(id) {
+      // todo: utils
+      function escape(html) {
+        const text = document.createTextNode(html)
+        const container = document.createElement('p')
+        container.appendChild(text)
+        return container.innerHTML
+      }
+
+      const { $toast } = this
+      this.$dialog.confirm({
+        message: `Are you sure to remove ${escape(id)}`,
+        type: 'is-danger',
+        hasIcon: true,
+        onConfirm(value) {
+          // TODO: path traversal?
+          axios.delete('/device/' + id.replace(/^tcp@/, ''), value)
+            .catch(err => $toast.open({
+              message: `failed to remove device`,
+              type: 'is-danger'
+            }))
         }
       });
     },
@@ -98,8 +125,17 @@ export default {
 
 <style scoped>
 .add {
-  padding: 0.5em 0.75em;
+  margin-top: 0.5em;
+  padding: 0.25em 0.75em;
   display: block;
   cursor: pointer;
+}
+
+.remove {
+  cursor: pointer;
+  background: transparent;
+  border: none;
+  margin-top: -4px;
+  /* margin: 0.5em; */
 }
 </style>
